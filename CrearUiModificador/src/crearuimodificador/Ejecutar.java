de63dae0133d5fcef0706ad895cf4f688543b7a0
Paco G. <pacoggg@users.noreply.github.com>
2019-01-20 02:11:40 +0100

    creando el UI del instalador

---
+++ CrearUiModificador/src/crearuimodificador/Ejecutar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crearuimodificador;
+
+/**
+ *
+ * @author Paco G
+ */
+public class Ejecutar {
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+         new ModificadorUi().setVisible(true);
+     }
+}
